b5ec6a85857a65dc8b65546dbb47d66c40489cf1
epjava7 <ericpanjava7@gmail.com>
2025-08-08 12:39:20 -0700

    fixed main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -163,7 +163,7 @@ resource "aws_route_table" "private" {
   vpc_id = aws_vpc.this.id
   route {
     cidr_block = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat.id
+    nat_gateway_id = aws_nat_gateway.gw.id
   }
 }
 
